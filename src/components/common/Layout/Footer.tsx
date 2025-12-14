3939de05d2fd6bce63cf2cb45d3de318418aa1d8
gaye-00 <gayeabdoulaye163@gmail.com>
2025-12-14 02:36:02 +0000

    ajout docker-compose v3

--- src/components/common/Layout/Footer.tsx
+++ src/components/common/Layout/Footer.tsx
@@ -71,10 +71,15 @@ const Footer = () => {
     visible: {
       opacity: 1,
       y: 0,
+      // transition: {
+      //   duration: 0.5,
+      //   // ease: "easeOut",
+      //   ease: [0.6, 0.01, 0.05, 0.95],
+      // },
       transition: {
         duration: 0.5,
-        // ease: "easeOut",
-        ease: [0.6, 0.01, 0.05, 0.95],
+        type: "spring",
+        stiffness: 100,
       },
     },
   };
